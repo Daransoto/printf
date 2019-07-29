76318c5a3fdd2ec62f6cd0b338624023e229a36c
Daransoto <901@holbertonschool.com>
2019-07-29 00:05:56 +0000

    Function that prints numbers

---
+++ print_number.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+* print_number - Prints an integer.
+* @n: integer to be printed.
+* Return: Number of chars printed.
+*/
+unsigned int print_number(int n)
+{
+	unsigned int d = 1, c, r, chars = 0;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+		chars++;
+	}
+	c = n;
+	while (c / 10)
+	{
+		d *= 10;
+		c /= 10;
+	}
+	_putchar(c + '0');
+	chars++;
+	while (d >= 10)
+	{
+		r = n % d;
+		d /= 10;
+		_putchar(r / d + '0');
+		chars++;
+	}
+	return (chars);
+}
