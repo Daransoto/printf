772d6e90ca0f8899fab3b65739128cd4b3ab0296
thomas <tomasmontoya123@gmail.com>
2019-07-29 14:20:25 -0500

    encodes into rot13

---
+++ rot13.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes into root13
+ * @cypher: translation
+ *   (* a blank line
+ *    * Description: Longer description of the function)?
+ *    (* section header: Section description)*
+ *     * Return: printcounter
+ *
+ */
+int rot13(char *cypher)
+{
+	int a;
+	int b;
+	int printCounter = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char trans[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+
+	for (a = 0; cypher[a] != '\0'; a++)
+	{
+		for (b = 0; b <= 52; b++)
+		{
+			if (cypher[a] == in[b])
+			{
+				cypher[a] = trans[b];
+				_putchar(cypher[a]);
+				printCounter++;
+				break;
+			}
+		}
+	}
+	return (printCounter);
+}
+
