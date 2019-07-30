59bea7c3fb07087e8cb739eb4dac1260d7a9ebd9
thomas <tomasmontoya123@gmail.com>
2019-07-30 11:16:42 -0500

    handle the p specifier

---
+++ case_p.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* case_p - print address
+* @ptr: pointer
+* Return: print counts
+*/
+int case_p(int *ptr)
+{
+	int counter = 0;
+
+	counter += write(1, "0x", 2);
+	counter += _printf("%x", ptr);
+	return (counter);
+}
